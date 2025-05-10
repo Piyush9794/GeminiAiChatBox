035e170ebbb76af57dd10bb5d1b9f2c1afdd1a27
Piyush9794 <vishwakarmapiyush327@gmail.com>
2025-05-10 17:49:14 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this if you are hosting on a sub-path (e.g., '/subfolder/')
-})
+  base: '/GeminiAiChatBox/', // Ensure this is correct for your project
+});
+
