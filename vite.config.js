5ec89b30ab35efaf575c20b20970e8c65f75f9f8
Piyush9794 <vishwakarmapiyush327@gmail.com>
2025-05-11 16:52:08 +0530

    fix file refrence

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        entryFileNames: `assets/[name].js`, // Ensures .js extension
-      }
-    }
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 })
